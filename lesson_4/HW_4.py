3265014227d7ff6867985e3c4a0c71475e5fe5ec
Kalashnyk Artem <kalart@ukr.net>
2023-04-24 18:50:56 +0300

    adjustmentd HW_4

--- lesson_4/HW_4.py
+++ lesson_4/HW_4.py
@@ -10,46 +10,44 @@ def players_repr():
     opt = list(team[0].keys())
     field = str(input(f"Enter your choice {opt} or all information: "))
     if field == "all information":
-        count_ = 0
+        players_amount = 0
         for player in team:
-            count_ += 1
-            print(f"Player № {count_}: {player}")
+            players_amount += 1
+            print(f"Player № {players_amount}: {player}")
     else:
-        list_ = []
+        fields = []
         for player in team:
-            list_.append(player.get(field))
-        print(f"List of players by {field}: {list_}")
+            fields.append(player.get(field))
+        print(f"List of players by {field}: {fields}")
 
 
 def players_add():
-    pl_name = str(input("Enter player name: "))
+    pl_name = input("Enter player name: ")
     pl_age = int(input("Enter player age: "))
     pl_numb = int(input("Enter player number: "))
-    list_numb = []
     for player in team:
-        list_numb.append(player.get("number"))
-    if pl_numb in list_numb:
-        print(f"Player with number № {pl_numb} already exists")
-    else:
-        new_pl = {"name": pl_name, "age": pl_age, "number": pl_numb}
-        team.append(new_pl)
-        print("Player added")
-        print(new_pl)
+        if player["number"] == pl_numb:
+            print(f"Player with number № {pl_numb} already exists")
+            break
+        else:
+            new_pl = {"name": pl_name, "age": pl_age, "number": pl_numb}
+            team.append(new_pl)
+            print("Player added")
+            print(new_pl)
+            break
 
 
 def players_del():
     num_player = int(input("Enter number of player: "))
-    list_ = []
     for player in team:
-        list_.append(player.get("number"))
-    if num_player not in list_:
-        print(f"Player with number № {num_player} does not exists")
-    else:
-        for player in team:
-            if num_player == player.get("number"):
-                team.remove(player)
-                print(f"Player with number № {num_player} is removed")
-                print(f"Line-up: {team}")
+        if num_player != player["number"]:
+            print(f"Player with number № {num_player} does not exists")
+            break
+        else:
+            team.remove(player)
+            print(f"Player with number № {num_player} is removed")
+            print(f"Line-up: {team}")
+            break
 
 
 def players_find():
@@ -59,11 +57,8 @@ def players_find():
     for player in team:
         if search_word == str(player[search_field]):
             print(player)
-    list_word = []
-    for player in team:
-        list_word.append(player.get(search_field))
-    if search_word not in list_word:
-        print("There is not player in the list")
+        else:
+            print("There is not player in the list")
 
 
 def players_get_by_name() -> dict:
@@ -73,11 +68,9 @@ def players_get_by_name() -> dict:
         if search_name == str(player["name"]):
             print(player)
             break
-    list_name = []
-    for player in team:
-        list_name.append(player.get("name"))
-    if search_name not in list_name:
-        print("There is not player in the list")
+        else:
+            print("There is not player in the list")
+            break
 
 
 def main():
@@ -86,17 +79,17 @@ def main():
         user_input = input(f"Enter your choice {options}: ")
         if not (user_input):
             break
-        if user_input == "repr":
+        elif user_input == "repr":
             players_repr()
-        if user_input == "add":
+        elif user_input == "add":
             players_add()
-        if user_input == "del":
+        elif user_input == "del":
             players_del()
-        if user_input == "find":
+        elif user_input == "find":
             players_find()
-        if user_input == "get":
+        elif user_input == "get":
             players_get_by_name()
-        if user_input == "exit":
+        elif user_input == "exit":
             break
 
 
